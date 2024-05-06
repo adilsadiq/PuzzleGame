c4d3e201c2382ae20671bb0d9ce15b2cd2e80bd7
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-06 08:43:48 -0400

    Update slidingoption.py

--- slidingoption.py
+++ slidingoption.py
@@ -5,8 +5,8 @@ class Slidingoption:
   def __init__(self, x, y):
     self.x = x
     self.y = y
+    DEFAULT_IMAGE_SIZE = (50, 50)
     self.image = pygame.image.load("Slidingoption.PNG")
+    self.image = pygame.transform.scale(self.image, DEFAULT_IMAGE_SIZE)
     self.image_size = self.image.get_size()
     self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-    
-  
