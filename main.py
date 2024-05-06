32ce2eb56a72de1e8dc2045152005ba4770f3371
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-06 08:35:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("this is my repository for a puzzle game")
+import pygame
+from slidingoption import Slidingoption
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Times New Roman', 15)
+pygame.display.set_caption("Puzzle Game")
+
+SCREEN_HEIGHT = 550
+SCREEN_WIDTH = 550
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+option_message = "Choose your puzzle!"
+display_option_message = my_font.render(option_message, True, (0, 0, 0))
+
+slidingpuzzleoption = Slidingoption(40, 40)
+
+r = 255
+g = 255
+b = 255
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+  
+    screen.fill((r, g, b))
+    screen.blit(display_option_message, (185, 0))
+    screen.blit(slidingpuzzleoption.image, slidingpuzzleoption.rect)
+
+    pygame.display.update()
+
+pygame.quit()
