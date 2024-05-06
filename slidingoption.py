ab4817feb782e24ca4aaaad083ee71834ba01ac0
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-06 08:36:59 -0400

    Add files via upload

---
+++ slidingoption.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Slidingoption:
+
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.image = pygame.image.load("Slidingoption.PNG")
+    self.image_size = self.image.get_size()
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    
+  
