3096c0f1267e972fc168a75011ec8b14857e2435
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-16 08:44:58 -0400

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -8,3 +8,11 @@ class Tile:
     self.image = pygame.transform.scale(self.image, (tilesizex, tilesizey))
     self.image_size = self.image.get_size()
     self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+  def move(self, new_x, new_y):
+    self.x = new_x
+    self.y = new_y
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+  def shrink_image(self, newtilesizex, newtilesizey):
+    self.image = pygame.transform.scale(self.image, (newtilesizex, newtilesizey))
+    self.image_size = self.image.get_size()
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
