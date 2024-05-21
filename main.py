5d00b0280681eb82278aa78fa982693574297a61
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-21 08:43:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Times New Roman', 15)
 pygame.display.set_caption("Puzzle Game")
 
-SCREEN_HEIGHT = 550
-SCREEN_WIDTH = 550
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 600
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 line_color = (0, 0, 0)
@@ -34,6 +34,7 @@ g = 255
 b = 255
 run = True
 start_game = False
+selected1 = False
 
 orderfornumber1 = (180, 100)
 orderfornumber2 = (360, 100)
@@ -46,7 +47,12 @@ orderfornumber8 = (360, 360)
 
 
 while run:
+
+
+        
+
     for event in pygame.event.get():
+        keys = pygame.key.get_pressed() 
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -54,36 +60,46 @@ while run:
                 start_game = True
             if number1.rect.collidepoint(event.pos) == True and start_game == True:
                 number1.shrink_image(170, 170)
-            else: 
-                number1.shrink_image(180, 180)
-            if start_game == True and number2.rect.collidepoint(event.pos):
-                number2.shrink_image(170, 170)
-            else:
-                number2.shrink_image(180, 180)
-            if start_game == True and number3.rect.collidepoint(event.pos):
-                number3.shrink_image(170, 170)
-            else:
-                number3.shrink_image(180, 180)
-            if start_game == True and number4.rect.collidepoint(event.pos):
-                number4.shrink_image(170, 170)
-            else:
-                number4.shrink_image(180, 180)
-            if start_game == True and number5.rect.collidepoint(event.pos):
-                number5.shrink_image(170, 170)
-            else:
-                number5.shrink_image(180, 180)
-            if start_game == True and number6.rect.collidepoint(event.pos):
-                number6.shrink_image(170, 170)
-            else:
-                number6.shrink_image(180, 180)
-            if start_game == True and number7.rect.collidepoint(event.pos):
-                number7.shrink_image(170, 170)
-            else:
-                number7.shrink_image(180, 180)
-            if start_game == True and number8.rect.collidepoint(event.pos):
-                number8.shrink_image(170, 170)
-            else: 
-                number8.shrink_image(180, 180)
+                selected1 = True
+        if keys[pygame.K_d] and selected1 == True:
+            number1 = Tile(0 + 200, 0, "number1.png", 180, 180)
+        elif keys[pygame.K_a] and selected1 == True:
+            number1 = Tile(0 - 200, 0, "number1.png", 180, 180)
+        elif keys[pygame.K_w] and selected1 == True:
+            number1 = Tile(0, 0 - 200, "number1.png", 180, 180)
+        elif keys[pygame.K_s] and selected1 == True:
+            number1 = Tile(0, 0 + 200, "number1.png", 180, 180)
+
+        # else: 
+        #     number1.shrink_image(200, 200)
+        # if start_game == True and number2.rect.collidepoint(event.pos):
+        #     number2.shrink_image(170, 170)
+        # else:
+        #     number2.shrink_image(200, 200)
+        # if start_game == True and number3.rect.collidepoint(event.pos):
+        #     number3.shrink_image(170, 170)
+        # else:
+        #     number3.shrink_image(200, 200)
+        # if start_game == True and number4.rect.collidepoint(event.pos):
+        #     number4.shrink_image(170, 170)
+        # else:
+        #     number4.shrink_image(200, 200)
+        # if start_game == True and number5.rect.collidepoint(event.pos):
+        #     number5.shrink_image(170, 170)
+        # else:
+        #     number5.shrink_image(200, 200)
+        # if start_game == True and number6.rect.collidepoint(event.pos):
+        #     number6.shrink_image(170, 170)
+        # else:
+        #     number6.shrink_image(200, 200)
+        # if start_game == True and number7.rect.collidepoint(event.pos):
+        #     number7.shrink_image(170, 170)
+        # else:
+        #     number7.shrink_image(200, 200)
+        # if start_game == True and number8.rect.collidepoint(event.pos):
+        #     number8.shrink_image(170, 170)
+        # else: 
+        #     number8.shrink_image(200, 200)
                 
     if start_game == False:
         screen.fill((r, g, b))
@@ -92,20 +108,19 @@ while run:
     if start_game == True:
         screen.fill((r, g, b))
         screen.blit(number1.image, number1.rect)
-        screen.blit(number2.image, number2.rect)
-        screen.blit(number3.image, number3.rect)
-        screen.blit(number4.image, number4.rect)
-        screen.blit(number5.image, number5.rect)
-        screen.blit(number6.image, number6.rect)
-        screen.blit(number7.image, number7.rect)
-        screen.blit(number8.image, number8.rect)
+        # screen.blit(number2.image, number2.rect)
+        # screen.blit(number3.image, number3.rect)
+        # screen.blit(number4.image, number4.rect)
+        # screen.blit(number5.image, number5.rect)
+        # screen.blit(number6.image, number6.rect)
+        # screen.blit(number7.image, number7.rect)
+        # screen.blit(number8.image, number8.rect)
         
-        pygame.draw.line(screen, line_color, (0, 0), (0, 550), width = 3)
-        pygame.draw.line(screen, line_color, (0, 0), (550, 0), width = 5)
-        pygame.draw.line(screen, line_color, (550, 0), (550, 550), width = 5)
-        pygame.draw.line(screen, line_color, (0, 550), (550, 550), width = 5)
+        pygame.draw.line(screen, line_color, (0, 0), (0, 600), width = 3)
+        pygame.draw.line(screen, line_color, (0, 0), (600, 0), width = 5)
+        pygame.draw.line(screen, line_color, (600, 0), (600, 600), width = 5)
+        pygame.draw.line(screen, line_color, (0, 600), (600, 600), width = 5)
 
     pygame.display.update()
 
 pygame.quit()
-
