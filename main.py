753b7f25582fb077a9bab260661c23d35fbe8b62
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-05-29 08:40:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,16 +35,8 @@ g = 255
 b = 255
 run = True
 start_game = False
-selected1 = False
 
-orderfornumber1 = (180, 100)
-orderfornumber2 = (360, 100)
-orderfornumber3 = (0, 100)
-orderfornumber4 = (180, 180)
-orderfornumber5 = (360, 180)
-orderfornumber6 = (0, 180)
-orderfornumber7 = (180, 360)
-orderfornumber8 = (360, 360)
+
 moving = False
 
 while run:
@@ -58,56 +50,49 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if spsb.rect.collidepoint(event.pos):
                 start_game = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if number1.rect.collidepoint(event.pos) == True and start_game == True:
-                number1.shrink_image(170, 170)
-                selected1 = True
+        while start_game == True:
+            if event.type == pygame.MOUSEBUTTONDOWN and number1.rect.collidepoint(event.pos):
                 moving = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                moving = False
-            elif event.type == MOUSEMOTION and moving == True:
-                number1.move(event.pos[0] - number1.image_size[0])
-                # number1.move_ip(event.rel)  
-
-        # if keys[pygame.K_d] and selected1 == True:
-
-        # elif keys[pygame.K_a] and selected1 == True:
-        #     number1 = Tile(0 - 200, 0, "number1.png", 180, 180)
-        # elif keys[pygame.K_w] and selected1 == True:
-        #     number1 = Tile(0, 0 - 200, "number1.png", 180, 180)
-        # elif keys[pygame.K_s] and selected1 == True:
-        #     number1 = Tile(0, 0 + 200, "number1.png", 180, 180)
-
-        # else: 
-        #     number1.shrink_image(200, 200)
-        # if start_game == True and number2.rect.collidepoint(event.pos):
-        #     number2.shrink_image(170, 170)
-        # else:
-        #     number2.shrink_image(200, 200)
-        # if start_game == True and number3.rect.collidepoint(event.pos):
-        #     number3.shrink_image(170, 170)
-        # else:
-        #     number3.shrink_image(200, 200)
-        # if start_game == True and number4.rect.collidepoint(event.pos):
-        #     number4.shrink_image(170, 170)
-        # else:
-        #     number4.shrink_image(200, 200)
-        # if start_game == True and number5.rect.collidepoint(event.pos):
-        #     number5.shrink_image(170, 170)
-        # else:
-        #     number5.shrink_image(200, 200)
-        # if start_game == True and number6.rect.collidepoint(event.pos):
-        #     number6.shrink_image(170, 170)
-        # else:
-        #     number6.shrink_image(200, 200)
-        # if start_game == True and number7.rect.collidepoint(event.pos):
-        #     number7.shrink_image(170, 170)
-        # else:
-        #     number7.shrink_image(200, 200)
-        # if start_game == True and number8.rect.collidepoint(event.pos):
-        #     number8.shrink_image(170, 170)
-        # else: 
-        #     number8.shrink_image(200, 200)
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number1.move(x,y) 
+            elif event.type == pygame.MOUSEBUTTONDOWN and number2.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number2.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number3.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number3.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number4.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number4.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number5.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number5.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number6.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number6.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number7.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number7.move(x,y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and number8.rect.collidepoint(event.pos):
+                moving = True
+                if event.type == MOUSEMOTION:
+                    x,y = pygame.mouse.get_pos()
+                    number8.move(x,y)
+                
+        
                 
     if start_game == False:
         screen.fill((r, g, b))
@@ -116,13 +101,13 @@ while run:
     if start_game == True:
         screen.fill((r, g, b))
         screen.blit(number1.image, number1.rect)
-        # screen.blit(number2.image, number2.rect)
-        # screen.blit(number3.image, number3.rect)
-        # screen.blit(number4.image, number4.rect)
-        # screen.blit(number5.image, number5.rect)
-        # screen.blit(number6.image, number6.rect)
-        # screen.blit(number7.image, number7.rect)
-        # screen.blit(number8.image, number8.rect)
+        screen.blit(number2.image, number2.rect)
+        screen.blit(number3.image, number3.rect)
+        screen.blit(number4.image, number4.rect)
+        screen.blit(number5.image, number5.rect)
+        screen.blit(number6.image, number6.rect)
+        screen.blit(number7.image, number7.rect)
+        screen.blit(number8.image, number8.rect)
         
         pygame.draw.line(screen, line_color, (0, 0), (0, 600), width = 3)
         pygame.draw.line(screen, line_color, (0, 0), (600, 0), width = 5)
