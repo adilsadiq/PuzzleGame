7b5ddd4ec832b13265e317a41dd976e2b93bae07
adilsadiq <167536562+adilsadiq@users.noreply.github.com>
2024-06-11 18:49:20 -0400

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -1,18 +1,19 @@
 import pygame
 
-class Tile:      
-  def __init__(self, x, y, image, tilesizex, tilesizey):
-    self.x = x
-    self.y = y
-    self.image = pygame.image.load(image)
-    self.image = pygame.transform.scale(self.image, (tilesizex, tilesizey))
-    self.image_size = self.image.get_size()
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-  def move(self, new_x, new_y):
-    self.x = new_x
-    self.y = new_y
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-  def shrink_image(self, newtilesizex, newtilesizey):
-    self.image = pygame.transform.scale(self.image, (newtilesizex, newtilesizey))
-    self.image_size = self.image.get_size()
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+class Tile:
+    def __init__(self, x, y, image, tilesizex, tilesizey):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (tilesizex, tilesizey))
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.select = False
+    def move(self, screen):
+        if self.select:
+            self.rect.center = pygame.mouse.get_pos()
+        screen.blit(self.image, self.rect)
+    def shrink_image(self, newtilesizex, newtilesizey):
+        self.image = pygame.transform.scale(self.image, (newtilesizex, newtilesizey))
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
